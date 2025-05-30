6c55221f62c59a19156c4b11503788dd8488904d
JesusRamirez <jesusemmanuelramirez@hotmail.com>
2025-05-30 11:47:56 -0600

    Update the landing page
    Update the landing page

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             if (isMobile) {
                 img.style.objectFit = 'contain'; 
-                img.style.maxHeight = '100vh';    // Tenia 80% pero al ponerle 100vh se arreglo jajaj
+                img.style.maxHeight = '100vh'; 
                 img.style.height = 'auto';       
                 item.style.height = 'auto';      
             } else {
@@ -68,52 +68,32 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 
-    
+  
+    const captions = {
+    cumpleanos: ["Cumpleaños 1", "Cumpleaños 2", "Cumpleaños 3"],
+    babyshower: ['<a href="./templates/BabyShower_1.html">Baby Shower 1</a>', "Baby Shower 2", "Baby Shower 3"],
+    xvanos: ['<a href="./templates/XV_1.html">XV Años 1</a>', '<a href="./templates/XV_2.html">XV Años 2</a>', '<a href="./templates/XV_3.html">XV Años 3</a>'],
+    bodas: ["Bodas 1", "Bodas 2", "Bodas 3"]
+    };
+
+    function createSwiper(selector, captionsId, captionsArr) {
+        const swiper = new Swiper(selector, {
+        effect: "cards",
+        grabCursor: true,
+        on: {
+            slideChange: function () {
+            const idx = this.activeIndex;
+            document.getElementById(captionsId).innerHTML = captionsArr[idx];
+            },
+        },
+        });
+        return swiper;
+    }
+
+    createSwiper(".cumpleanosSwiper", "cumpleCaption", captions.cumpleanos);
+    createSwiper(".babyshowerSwiper", "babyshowerCaption", captions.babyshower);
+    createSwiper(".xvanosSwiper", "xvanosCaption", captions.xvanos);
+    createSwiper(".bodasSwiper", "bodasCaption", captions.bodas);
 });
-
-
-const dots = document.querySelectorAll('.dot');
-let currentIndex = 0;
-const cards = document.querySelectorAll('.template-cards .card');
-
-// actualizar los puntos
-function updateDots() {
-    dots.forEach((dot, index) => {
-        if (index === currentIndex) {
-            dot.classList.add('active');
-        } else {
-            dot.classList.remove('active');
-        }
-    });
-}
-
-//navegar entre las tarjetas
-dots.forEach((dot, index) => {
-    dot.addEventListener('click', () => {
-        currentIndex = index;
-        updateCards();
-        updateDots();
-    });
-});
-
-//actualizar las tarjetas
-function updateCards() {
-    const itemsPerPage = 4;
-    const start = currentIndex * itemsPerPage;
-    const end = start + itemsPerPage;
-
-    cards.forEach((card, index) => {
-        if (index >= start && index < end) {
-            card.style.display = 'block';
-        } else {
-            card.style.display = 'none';
-        }
-    });
-}
-
-updateCards();
-updateDots();
-
-
 
 
