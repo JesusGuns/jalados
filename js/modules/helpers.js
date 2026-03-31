e3a3a272ab2e5b2798e0e1a66dbe63d73d375f34
JesusRamirez <jesusemmanuelramirez@hotmail.com>
2026-03-31 12:13:27 -0600

    Update Wedding 2
    Add Confirmation Service

--- js/modules/helpers.js
+++ js/modules/helpers.js
@@ -14,48 +14,78 @@ define(["jquery", "extended"], function ($) {
     IconFeast: "https://cdn-icons-png.flaticon.com/128/4156/4156252.png",
     IconVals: "https://cdn-icons-png.flaticon.com/128/1483/1483389.png",
     IconSuit: "https://cdn-icons-png.flaticon.com/128/389/389573.png",
-    IconDress: "https://cdn-icons-png.flaticon.com/128/3507/3507059.png"
-  }
+    IconDress: "https://cdn-icons-png.flaticon.com/128/3507/3507059.png",
+  };
 
   var Resources = {
-    MainEvent : {},
+    MainEvent: {},
     SecondaryEvent: {},
     GoogleCalendar: {},
-    AdditionalInformation : {},
-    StaticLinks: staticLinks
+    AdditionalInformation: {},
+    StaticLinks: staticLinks,
   };
   var module = {
     init: function (resources) {
       Resources.MainEvent = resources.MainEvent;
-      Resources.MainEvent.Time =  Resources.MainEvent.Date.getTime();
-      Resources.MainEvent.DateString =  module.onGetDateString(Resources.MainEvent.Date);
-      Resources.MainEvent.HourString =  module.onGetHoursString(Resources.MainEvent.Date);
-      Resources.MainEvent.WeekDay =  module.onGetWeekDay(Resources.MainEvent.Date);
-      Resources.MainEvent.Day =  module.onGetDay(Resources.MainEvent.Date);
-      Resources.MainEvent.Month =  module.onGetMonth(Resources.MainEvent.Date);
-      Resources.MainEvent.Year =  module.onGetYear(Resources.MainEvent.Date);
-      Resources.MainEvent.YYYYMMDD =  module.onGetDateYYYYMMDD(Resources.MainEvent.Date);
-      Resources.MainEvent.RandomCollageImage= 0;
+      Resources.MainEvent.Time = Resources.MainEvent.Date.getTime();
+      Resources.MainEvent.DateString = module.onGetDateString(
+        Resources.MainEvent.Date,
+      );
+      Resources.MainEvent.HourString = module.onGetHoursString(
+        Resources.MainEvent.Date,
+      );
+      Resources.MainEvent.WeekDay = module.onGetWeekDay(
+        Resources.MainEvent.Date,
+      );
+      Resources.MainEvent.Day = module.onGetDay(Resources.MainEvent.Date);
+      Resources.MainEvent.Month = module.onGetMonth(Resources.MainEvent.Date);
+      Resources.MainEvent.Year = module.onGetYear(Resources.MainEvent.Date);
+      Resources.MainEvent.YYYYMMDD = module.onGetDateYYYYMMDD(
+        Resources.MainEvent.Date,
+      );
+      Resources.MainEvent.RandomCollageImage = 0;
 
       Resources.SecondaryEvent = resources.SecondaryEvent;
-      Resources.SecondaryEvent.Time= Resources.SecondaryEvent.Date.getTime();
-      Resources.SecondaryEvent.DateString= module.onGetDateString(Resources.SecondaryEvent.Date);
-      Resources.SecondaryEvent.HourString= module.onGetHoursString(Resources.SecondaryEvent.Date);
-      Resources.SecondaryEvent.WeekDay= module.onGetWeekDay(Resources.SecondaryEvent.Date);
-      Resources.SecondaryEvent.Day= module.onGetDay(Resources.SecondaryEvent.Date);
-      Resources.SecondaryEvent.Month= module.onGetMonth(Resources.SecondaryEvent.Date);
-      Resources.SecondaryEvent.Year= module.onGetYear(Resources.SecondaryEvent.Date);
-      Resources.SecondaryEvent.YYYYMMDD= module.onGetDateYYYYMMDD(Resources.SecondaryEvent.Date);
+      Resources.SecondaryEvent.Time = Resources.SecondaryEvent.Date.getTime();
+      Resources.SecondaryEvent.DateString = module.onGetDateString(
+        Resources.SecondaryEvent.Date,
+      );
+      Resources.SecondaryEvent.HourString = module.onGetHoursString(
+        Resources.SecondaryEvent.Date,
+      );
+      Resources.SecondaryEvent.WeekDay = module.onGetWeekDay(
+        Resources.SecondaryEvent.Date,
+      );
+      Resources.SecondaryEvent.Day = module.onGetDay(
+        Resources.SecondaryEvent.Date,
+      );
+      Resources.SecondaryEvent.Month = module.onGetMonth(
+        Resources.SecondaryEvent.Date,
+      );
+      Resources.SecondaryEvent.Year = module.onGetYear(
+        Resources.SecondaryEvent.Date,
+      );
+      Resources.SecondaryEvent.YYYYMMDD = module.onGetDateYYYYMMDD(
+        Resources.SecondaryEvent.Date,
+      );
 
       Resources.GoogleCalendar = resources.GoogleCalendar;
       Resources.AdditionalInformation = resources.AdditionalInformation;
       module.onLoad();
     },
-    onGetResources:function(){
+    onGetResources: function () {
       return Resources;
     },
     onGetWeekDay: function (eventDate) {
-      const days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+      const days = [
+        "Domingo",
+        "Lunes",
+        "Martes",
+        "Miércoles",
+        "Jueves",
+        "Viernes",
+        "Sábado",
+      ];
       const dayOfWeek = eventDate.getDay();
       const dayName = days[dayOfWeek];
       return dayName;
@@ -64,7 +94,20 @@ define(["jquery", "extended"], function ($) {
       return eventDate.getDate();
     },
     onGetMonth: function (eventDate) {
-      const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+      const months = [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre",
+      ];
       const month = months[eventDate.getMonth()];
       return month;
     },
@@ -76,7 +119,9 @@ define(["jquery", "extended"], function ($) {
       const dayName = module.onGetWeekDay(eventDate);
       const month = module.onGetMonth(eventDate);
       const year = module.onGetYear(eventDate);
-      const dateString = includeDayName ? `${dayName} ${day} de ${month} de ${year}` : `${day} de ${month} de ${year}`;
+      const dateString = includeDayName
+        ? `${dayName} ${day} de ${month} de ${year}`
+        : `${day} de ${month} de ${year}`;
       return dateString;
     },
     onGetHoursString: function (eventDate, includeThe) {
@@ -85,7 +130,9 @@ define(["jquery", "extended"], function ($) {
       const mins = eventDate.getMinutes().toString().padStart(2, "0");
       const the = hours > 1 ? "las " : "la";
       hours = hours % 12 || 12;
-      const hourString = includeThe ? `${the} ${hours}:${mins} ${ampm}` : `${hours}:${mins} ${ampm}`;
+      const hourString = includeThe
+        ? `${the} ${hours}:${mins} ${ampm}`
+        : `${hours}:${mins} ${ampm}`;
       return hourString;
     },
     onGetDateYYYYMMDD: function (eventDate) {
@@ -102,7 +149,9 @@ define(["jquery", "extended"], function ($) {
       const now = new Date().getTime();
       const distance = eventDate - now;
       const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+      );
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
       $("#days").text(module.onAddZeroToNumber(days));
@@ -118,7 +167,8 @@ define(["jquery", "extended"], function ($) {
     onGoogleCalendar: function (googleCalendar) {
       const base = "https://calendar.google.com/calendar/render";
       const calendarDate = module.onGetDateYYYYMMDD(Resources.MainEvent.Date);
-      const title = Resources.MainEvent.Description + " " + Resources.MainEvent.Name;
+      const title =
+        Resources.MainEvent.Description + " " + Resources.MainEvent.Name;
       const location = Resources.MainEvent.LocationDescription;
       const hour = Resources.MainEvent.HourString;
 
@@ -144,18 +194,54 @@ define(["jquery", "extended"], function ($) {
     onPlay: function () {
       $("#audioButton").click();
     },
-    onMoveElementFromArray: function(array, oldIndex, newIndex){
+    onMoveElementFromArray: function (array, oldIndex, newIndex) {
       if (newIndex >= array.length) {
         newIndex = array.length - 1;
       } else if (newIndex < 0) {
-        newIndex = 0
+        newIndex = 0;
       }
       array.splice(newIndex, 0, array.splice(oldIndex, 1)[0]);
       return array;
     },
+    onValidateToken: function (sheetWebAppURL, token) {
+      if (!token) return;
+      
+      fetch(sheetWebAppURL + "?token=" + token)
+        .then((resp) => resp.json())
+        .then((data) => {
+          debugger
+          if (data.exists) {
+          }
+        })
+      .catch((err) => {});
+    },
+    onSendConfirmation: function (sheetWebAppURL, confirm) {
+      const params = new URLSearchParams(window.location.search);
+      const token = params.get("token") || "";
+      if (!token) return;
+
+      fetch(sheetWebAppURL, {
+        method: "POST",
+        body: JSON.stringify({ token: token, confirmado: confirm }),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          if (data.status === "ok") {
+            debugger;
+          }
+        })
+        .catch((err) => {});
+    },
     onLoad: function () {
-      Resources.MainEvent.RandomCollageImage = Math.floor(Math.random() * Resources.MainEvent.CollageImages.length);
-      Resources.MainEvent.CollageImages = module.onMoveElementFromArray(Resources.MainEvent.CollageImages, Resources.MainEvent.RandomCollageImage, 3 );
+      Resources.MainEvent.RandomCollageImage = Math.floor(
+        Math.random() * Resources.MainEvent.CollageImages.length,
+      );
+      Resources.MainEvent.CollageImages = module.onMoveElementFromArray(
+        Resources.MainEvent.CollageImages,
+        Resources.MainEvent.RandomCollageImage,
+        3,
+      );
       //Main Event
       $("[data-replace=EventDateString]").text(Resources.MainEvent.DateString);
       $("[data-replace=EventHourString]").text(Resources.MainEvent.HourString);
@@ -164,49 +250,128 @@ define(["jquery", "extended"], function ($) {
       $("[data-replace=EventMother]").text(Resources.MainEvent.Mother);
       $("[data-replace=EventGodFather]").text(Resources.MainEvent.GodFather);
       $("[data-replace=EventGodMother]").text(Resources.MainEvent.GodMother);
-      $("[data-replace=EventChurchUrl]").attr("href", Resources.MainEvent.GoogleMapsUrl);
-      $("[data-replace=Image_MyParents]").attr("src", Resources.MainEvent.ImageMyParents);
-      $("[data-replace=Image_MyGodParents]").attr("src", Resources.MainEvent.ImageMyGodParents);
-      $("[data-replace=Image_DressCodeUrl]").attr("src", Resources.MainEvent.ImageDressCodeUrl);
-      $("[data-replace=EventAmazonGiftTableUrl]").attr("href", Resources.MainEvent.AmazonGiftTableUrl);
-      $("[data-replace=EventLiverpoolGiftTableUrl]").attr("href", Resources.MainEvent.LiverpoolGiftTableUrl);
+      $("[data-replace=EventChurchUrl]").attr(
+        "href",
+        Resources.MainEvent.GoogleMapsUrl,
+      );
+      $("[data-replace=Image_MyParents]").attr(
+        "src",
+        Resources.MainEvent.ImageMyParents,
+      );
+      $("[data-replace=Image_MyGodParents]").attr(
+        "src",
+        Resources.MainEvent.ImageMyGodParents,
+      );
+      $("[data-replace=Image_DressCodeUrl]").attr(
+        "src",
+        Resources.MainEvent.ImageDressCodeUrl,
+      );
+      $("[data-replace=EventAmazonGiftTableUrl]").attr(
+        "href",
+        Resources.MainEvent.AmazonGiftTableUrl,
+      );
+      $("[data-replace=EventLiverpoolGiftTableUrl]").attr(
+        "href",
+        Resources.MainEvent.LiverpoolGiftTableUrl,
+      );
 
-      $("[data-replace=MainEventUrl]").attr("href", Resources.MainEvent.GoogleMapsUrl);
-      $("[data-replace=MainEventHourString]").text(Resources.MainEvent.HourString);
+      $("[data-replace=MainEventUrl]").attr(
+        "href",
+        Resources.MainEvent.GoogleMapsUrl,
+      );
+      $("[data-replace=MainEventHourString]").text(
+        Resources.MainEvent.HourString,
+      );
       $("[data-replace=MainEventDay]").text(Resources.MainEvent.Day);
       $("[data-replace=MainEventWeekDay]").text(Resources.MainEvent.WeekDay);
       $("[data-replace=MainEventMonth]").text(Resources.MainEvent.Month);
       $("[data-replace=MainEventYear]").text(Resources.MainEvent.Year);
 
-      $("[data-replace=MainEventLocationDescription]").text(Resources.MainEvent.LocationDescription);
+      $("[data-replace=MainEventLocationDescription]").text(
+        Resources.MainEvent.LocationDescription,
+      );
       $("[data-replace=MainEventAddress]").text(Resources.MainEvent.Address);
 
       //Secondary Event
-      $("[data-replace=EventReceptionHourString]").text(Resources.SecondaryEvent.HourString);
-      $("[data-replace=EventReceptionUrl]").attr("href", Resources.SecondaryEvent.GoogleMapsUrl);
-      $("[data-replace=SecondaryEventLocationDescription]").text(Resources.SecondaryEvent.LocationDescription);
-      $("[data-replace=SecondaryEventAddress]").text(Resources.SecondaryEvent.Address);
+      $("[data-replace=EventReceptionHourString]").text(
+        Resources.SecondaryEvent.HourString,
+      );
+      $("[data-replace=EventReceptionUrl]").attr(
+        "href",
+        Resources.SecondaryEvent.GoogleMapsUrl,
+      );
+      $("[data-replace=SecondaryEventLocationDescription]").text(
+        Resources.SecondaryEvent.LocationDescription,
+      );
+      $("[data-replace=SecondaryEventAddress]").text(
+        Resources.SecondaryEvent.Address,
+      );
 
       //Google calendar
-      $("[data-replace=GoogleCalendarUrl]").attr("href", module.onGoogleCalendar(Resources.GoogleCalendar));
+      $("[data-replace=GoogleCalendarUrl]").attr(
+        "href",
+        module.onGoogleCalendar(Resources.GoogleCalendar),
+      );
 
       //Additional info
-      $("[data-replace=AudioUrl]").attr("src", Resources.AdditionalInformation.AudioUrl);
-      $("[data-replace=Image_FrontCard]").attr("src", Resources.AdditionalInformation.ImageFrontCard);
-      $("[data-replace=WhatsAppUrl]").attr("href", Resources.AdditionalInformation.WhatsAppUrl);
+      $("[data-replace=AudioUrl]").attr(
+        "src",
+        Resources.AdditionalInformation.AudioUrl,
+      );
+      $("[data-replace=Image_FrontCard]").attr(
+        "src",
+        Resources.AdditionalInformation.ImageFrontCard,
+      );
+      $("[data-replace=WhatsAppUrl]").attr(
+        "href",
+        Resources.AdditionalInformation.WhatsAppUrl,
+      );
 
       //static links
-      $("[data-replace=Image_AudioUrl]").attr("src", Resources.StaticLinks.ImageAudioUrl);
-      $("[data-replace=Image_AmazonUrl]").attr("src", Resources.StaticLinks.ImageAmazonUrl);
-      $("[data-replace=Image_LiverpoolUrl]").attr("src", Resources.StaticLinks.ImageLiverpoolUrl);
-      $("[data-replace=Image_GoogleLocation]").attr("src", Resources.StaticLinks.ImageGoogleLocation);
-      $("[data-replace=ActivityIconChurch]").attr("src", Resources.StaticLinks.IconChurch);
-      $("[data-replace=ActivityIconReception]").attr("src", Resources.StaticLinks.IconReception);
-      $("[data-replace=ActivityIconPhotSession]").attr("src", Resources.StaticLinks.IconPhotSession);
-      $("[data-replace=ActivityIconFeast]").attr("src", Resources.StaticLinks.IconFeast);
-      $("[data-replace=ActivityIconVals]").attr("src", Resources.StaticLinks.IconVals);
-      $("[data-replace=ActivityIconSuit]").attr("src", Resources.StaticLinks.IconSuit);
-      $("[data-replace=ActivityIconDress]").attr("src", Resources.StaticLinks.IconDress);
+      $("[data-replace=Image_AudioUrl]").attr(
+        "src",
+        Resources.StaticLinks.ImageAudioUrl,
+      );
+      $("[data-replace=Image_AmazonUrl]").attr(
+        "src",
+        Resources.StaticLinks.ImageAmazonUrl,
+      );
+      $("[data-replace=Image_LiverpoolUrl]").attr(
+        "src",
+        Resources.StaticLinks.ImageLiverpoolUrl,
+      );
+      $("[data-replace=Image_GoogleLocation]").attr(
+        "src",
+        Resources.StaticLinks.ImageGoogleLocation,
+      );
+      $("[data-replace=ActivityIconChurch]").attr(
+        "src",
+        Resources.StaticLinks.IconChurch,
+      );
+      $("[data-replace=ActivityIconReception]").attr(
+        "src",
+        Resources.StaticLinks.IconReception,
+      );
+      $("[data-replace=ActivityIconPhotSession]").attr(
+        "src",
+        Resources.StaticLinks.IconPhotSession,
+      );
+      $("[data-replace=ActivityIconFeast]").attr(
+        "src",
+        Resources.StaticLinks.IconFeast,
+      );
+      $("[data-replace=ActivityIconVals]").attr(
+        "src",
+        Resources.StaticLinks.IconVals,
+      );
+      $("[data-replace=ActivityIconSuit]").attr(
+        "src",
+        Resources.StaticLinks.IconSuit,
+      );
+      $("[data-replace=ActivityIconDress]").attr(
+        "src",
+        Resources.StaticLinks.IconDress,
+      );
     },
   };
   return module;
