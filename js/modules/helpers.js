0dc0d41e08d9d821dcac0c007efeb13a4ad518c0
JesusRamirez <jesusemmanuelramirez@hotmail.com>
2026-04-22 13:06:25 -0600

    Wedding2

--- js/modules/helpers.js
+++ js/modules/helpers.js
@@ -237,6 +237,7 @@ define(["jquery", "extended"], function ($) {
             if (data.confirmed) {
               $("._tableNumber").text(String(data.table).padStart(2, "0"));
               if($("body ._hide-section").length == 0){
+                $("body ._hide-tablesection").addClass("_hide-sectionn");
                 $("body ._hide-tablesection").removeClass("_hide-tablesection-section");
               }
             }
