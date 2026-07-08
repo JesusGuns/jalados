92ece00ba24e47c888dbd9abb19fdc3e3de888f7
JesusRamirez <jesusemmanuelramirez@hotmail.com>
2026-07-07 23:51:07 -0600

    Performance XV_1 Helpers

--- js/modules/helpers.js
+++ js/modules/helpers.js
@@ -1,6 +1,3 @@
-
-  import './extended.js';
-
   const staticLinks = {
     ImageAudioUrl: "../images/icons/play.png",
     ImageAudioPauseUrl: "../images/icons/pause.png",
@@ -323,4 +320,27 @@
       main.Gallery = helpers.onMoveElementFromArray(main.Gallery, randomIndex, 3);
     },
   };
+
+
+  $.fn.extend({
+    OnAudioPlayerClick: function ($audioButton) {
+      var audio = this[0];
+      if (audio.paused) {
+        var playPromise = audio.play();
+        
+        // Evita el error silencioso que congela el hilo
+        if (playPromise !== undefined) {
+          playPromise.catch(function (err) {
+            console.warn("Audio bloqueado por el navegador:", err);
+            $audioButton.addClass("off");
+          });
+        }
+        $audioButton.removeClass("off");
+      } else {
+        audio.pause();
+        $audioButton.addClass("off");
+      }
+    },
+  });
+
 
