15cdf061e29ff275a354a097f681479bf393e887
JesusRamirez <jesusemmanuelramirez@hotmail.com>
2026-07-12 17:56:33 -0600

    AppScript

--- api/sheet.js
+++ api/sheet.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "Token is required" });
   }
 
-  const baseUrl = "https://script.google.com/macros/s/AKfycbzHxyhpVJbnluZkPquzdGdREuSwYc5yXAejV287Rt_6oHjBVPQkAW0yUHLnCRhjP6nS/exec";
+  const baseUrl = "https://script.google.com/macros/s/AKfycbyNwJ0573iYs2Cx4BisEcb61-cAmQw2GSx3UUG6vJ5LRpLKrQGA7P7o0wCINcbA_Iys/exec";
 
   try {
     // GET
