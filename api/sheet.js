15f5c850fb7a90373d9f950e95fd2c54e7061060
JesusRamirez <jesusemmanuelramirez@hotmail.com>
2026-07-20 18:41:37 -0600

    Updates

--- api/sheet.js
+++ api/sheet.js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // Manejar preflight (IMPORTANTE para POST)
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
